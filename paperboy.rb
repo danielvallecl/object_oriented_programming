14bc38d654bd72efef4fdd4bf2d2afa978284c6f
danielvallecl <daniel.vallecl@gmail.com>
2016-06-06 13:10:36 -0400

    Finished Paperboy Assignment

--- paperboy.rb
+++ paperboy.rb
@@ -1,25 +1,42 @@
 #Paperboy Exercise#
 
 class Paperboy
-  attr_reader :name
-  def initialize(name, quota, experience, side, earnings)
+  attr_reader :name, :quota, :experience
+  def initialize(name, side)
   @name = name
-  @quota =  quota
-  @experience = experience
+  @quota =  50
+  @experience = 0
+  @earnings = 0
   @side = side
-  @earnings = earnings
 end
 
 def quota
-  return 50 + (@experience / 2)
+
 end
 
 def deliver(start_address, end_address)
+  even_side = 0
+  odd_side = 0
+  house_total = ((end_address - start_address) / 2.floor)
 
+  @experience = house_total
+
+  if house_total <= @quota
+    @earnings = @experience * (0.25)
+    @earnings -= 2 house_total < quota
+  elsif house_total > @quota
+    @earnings = (@experience * 0.25) + ((@experience - @quota) * 0.25)
+  end_address
 end
 
 def report
-  puts "I'm #{@name}, I've delivered #{@experience} papers and I've earned #{@earnings} so far!"
+  puts "I'm #{@name}, I've delivered #{@experience + @quota} papers
+  and I've earned #{@earnings} so far!"
 end
 
 end
+
+johnny = Paperboy.new("Johnny", "even")
+johnny.report
+johnny.deliver(205,500)
+johnny.report
